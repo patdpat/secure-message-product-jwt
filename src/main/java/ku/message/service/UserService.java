12e50b6576d75dabcc474ce686b6d6f6edc86150
patdpat <s.kietchai@outlook.com>
2021-05-07 21:08:46 +0700

    secure lab finalized

---
+++ src/main/java/ku/message/service/UserService.java
@@ -0,0 +1,39 @@
+package ku.message.service;
+
+import ku.message.model.User;
+import ku.message.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean isUsernameAvailable(String username) {
+        return repository.findByUsername(username) == null;
+    }
+
+    public int createUser(User user) {
+        User newUser = new User();
+        newUser.setFirstName(user.getFirstName());
+        newUser.setLastName(user.getLastName());
+        newUser.setUsername(user.getUsername());
+
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+
+        newUser.setPassword(hashedPassword);
+
+        repository.save(newUser);
+        return 1;
+    }
+
+    public User getUser(String username) {
+        return repository.findByUsername(username);
+    }
+}
